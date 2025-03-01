adcbe3b289d02c966162649ace43081761f7f52c
drastraea <sitorussamuel56@gmail.com>
2025-03-01 19:03:02 +0800

    Add Heroui and Framer Motion dependencies; update Tailwind config with Heroui plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import { heroui } from '@heroui/react'
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,10 +7,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [heroui()],
 }
 
