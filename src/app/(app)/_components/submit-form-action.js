e49a1800bdfcdbc57797f50e279b9664d11fdc46
dimasrangga <derangga1011@gmail.com>
2025-03-13 20:51:11 +0700

    add notification

--- src/app/(app)/_components/submit-form-action.js
+++ src/app/(app)/_components/submit-form-action.js
@@ -11,17 +11,25 @@ export async function submitFormAction(_, formData) {
   if (!session) return { success: false, message: "Please login to submit." };
 
   const itemData = extractFormData(formData);
-  if (!validateFormData(itemData)) return { success: false, message: "Please fill in all fields." };
+  if (!validateFormData(itemData))
+    return { success: false, message: "Please fill in all fields." };
 
   const coordinates = await getCoordinates(itemData.location);
-  if (!coordinates) return { success: false, message: "Location not found. Please enter a valid place." };
+  if (!coordinates)
+    return {
+      success: false,
+      message: "Location not found. Please enter a valid place.",
+    };
 
   const newItem = await createNewItem(itemData, coordinates, session.user.id);
   const imageUrl = await handleImageUpload(formData.get("file"), newItem.id);
   await processTags(imageUrl, newItem.id);
   await checkMatch(newItem, imageUrl);
 
-  return { success: true, message: `${itemData.type} ITEM SUBMITTED SUCCESSFULLY.` };
+  return {
+    success: true,
+    message: `${itemData.type} ITEM SUBMITTED SUCCESSFULLY.`,
+  };
 }
 
 function extractFormData(formData) {
@@ -81,14 +89,37 @@ async function checkMatch(singleItem, imageUrl) {
   const pairImageUrl = `${pairImage.images[0].itemId}/${pairImage.images[0].url}`;
   const imageComparison = await checkMatchImages(imageUrl, pairImageUrl);
 
-  const finalScore = (matchingStatus.matching_score + imageComparison.matching_score) / 2;
+  const finalScore =
+    (matchingStatus.matching_score + imageComparison.matching_score) / 2;
   if (finalScore >= 0.8) {
-    await createMatch(singleItem.id, matchingStatus.id, finalScore);
+    const match = await createMatch(
+      singleItem.id,
+      matchingStatus.id,
+      finalScore
+    );
+    if (singleItem.type === "LOST") {
+      await createNotification(singleItem.userId, match);
+    } else {
+      const item = await prisma.item.findUnique({
+        where: { id: matchingStatus.id, type: "LOST" },
+      });
+      await createNotification(item.userId, match.id);
+    }
   }
 }
 
 async function createMatch(lostItemId, foundItemId, score) {
   return await prisma.match.create({
     data: { lostItemId, foundItemId, score, status: "PENDING" },
+  });
+}
+
+async function createNotification(userId, matchId) {
+  await prisma.notification.create({
+    data: {
+      userId,
+      matchId,
+      message: "barangmu telah ditemukan",
+    },
   });
 }
