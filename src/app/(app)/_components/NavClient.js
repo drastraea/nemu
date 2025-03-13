c1fa71262830e2ab136f71c3ac9d193a0ff9801a
drastraea <sitorussamuel56@gmail.com>
2025-03-13 13:47:33 +0800

    refactor navigation component by extracting NavClient for improved structure

---
+++ src/app/(app)/_components/NavClient.js
@@ -0,0 +1,43 @@
+'use client';
+
+import { AvatarMenu } from './avatar-menu';
+import Image from 'next/image';
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    Button,
+} from '@heroui/react';
+
+export const NavClient = ({ session }) => {
+    return (
+        <Navbar height={64} maxWidth="xl" className="bg-black border-white/10 border-b">
+            <NavbarBrand>
+                <Link href="/">
+                    <Image alt="nemu logo" src="/images/nemu-w.png" width={70} height={35} />
+                </Link>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link href="/lost-item" className="text-white font-semibold">Lost Item</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link href="/found-item" className="text-white font-semibold">Found Item</Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem>
+                    {session ? (
+                        <AvatarMenu session={session} />
+                    ) : (
+                        <Button as={Link} href="/login" variant="bordered" className="text-white font-semibold">
+                            Login
+                        </Button>
+                    )}
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
+};
