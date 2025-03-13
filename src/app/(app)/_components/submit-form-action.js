5b91f7f1b99aa5b28cb13184a0e6239e2f089539
dimasrangga <derangga1011@gmail.com>
2025-03-13 20:52:48 +0700

    add log on process tag

--- src/app/(app)/_components/submit-form-action.js
+++ src/app/(app)/_components/submit-form-action.js
@@ -11,17 +11,25 @@ export async function submitFormAction(_, formData) {
   if (!session) return { success: false, message: "Please login to submit." };
 
   const itemData = extractFormData(formData);
-  if (!validateFormData(itemData)) return { success: false, message: "Please fill in all fields." };
+  if (!validateFormData(itemData))
+    return { success: false, message: "Please fill in all fields." };
 
   const coordinates = await getCoordinates(itemData.location);
-  if (!coordinates) return { success: false, message: "Location not found. Please enter a valid place." };
+  if (!coordinates)
+    return {
+      success: false,
+      message: "Location not found. Please enter a valid place.",
+    };
 
   const newItem = await createNewItem(itemData, coordinates, session.user.id);
   const imageUrl = await handleImageUpload(formData.get("file"), newItem.id);
   await processTags(imageUrl, newItem.id);
   await checkMatch(newItem, imageUrl);
 
-  return { success: true, message: `${itemData.type} ITEM SUBMITTED SUCCESSFULLY.` };
+  return {
+    success: true,
+    message: `${itemData.type} ITEM SUBMITTED SUCCESSFULLY.`,
+  };
 }
 
 function extractFormData(formData) {
@@ -57,15 +65,19 @@ async function handleImageUpload(file, itemId) {
 }
 
 async function processTags(imageUrl, itemId) {
-  const tags = await getTags(imageUrl);
-  await Promise.all(
-    Object.entries(tags).map(async ([type, name]) => {
-      if (name.toLowerCase() === "unknown") return;
-      let tag = await prisma.tag.findUnique({ where: { name } });
-      if (!tag) tag = await prisma.tag.create({ data: { name, type } });
-      await prisma.itemTag.create({ data: { itemId, tagId: tag.id } });
-    })
-  );
+  try {
+    const tags = await getTags(imageUrl);
+    await Promise.all(
+      Object.entries(tags).map(async ([type, name]) => {
+        if (name.toLowerCase() === "unknown") return;
+        let tag = await prisma.tag.findUnique({ where: { name } });
+        if (!tag) tag = await prisma.tag.create({ data: { name, type } });
+        await prisma.itemTag.create({ data: { itemId, tagId: tag.id } });
+      })
+    );
+  } catch (error) {
+    console.log(`processTags [ERROR]: ${error}`);
+  }
 }
 
 async function checkMatch(singleItem, imageUrl) {
@@ -81,7 +93,8 @@ async function checkMatch(singleItem, imageUrl) {
   const pairImageUrl = `${pairImage.images[0].itemId}/${pairImage.images[0].url}`;
   const imageComparison = await checkMatchImages(imageUrl, pairImageUrl);
 
-  const finalScore = (matchingStatus.matching_score + imageComparison.matching_score) / 2;
+  const finalScore =
+    (matchingStatus.matching_score + imageComparison.matching_score) / 2;
   if (finalScore >= 0.8) {
     await createMatch(singleItem.id, matchingStatus.id, finalScore);
   }
